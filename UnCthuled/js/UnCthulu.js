b977c88005d6f16698adba4b5d86bb7178dd6a4d
KEVIN SANZ FERNANDEZ <kevindevalencia@gmail.com>
2019-10-15 19:42:37 +0200

    Mapa creado y img insertadas

--- UnCthuled/js/UnCthulu.js
+++ UnCthuled/js/UnCthulu.js
@@ -1,13 +1,26 @@
-function matrizMapa() {
-    mapa = [];
+//posición inicial muñeco
 
-    for (let i = 0; i < 13; i++) {
-        mapa[i] = [];
-        for (let j = 0; j < 21; j++) {
+var mapa = new Array(14);
+for (let i = 0; i < mapa.length; i++) {
+    mapa[i] = new Array(21);
+}
+
+function matrizMapa() {
+
+
+
+
+    for (let i = 0; i < mapa.length; i++) {
+        for (let j = 0; j < mapa[i].length; j++) {
 
             var newDiv = document.createElement("div");
 
-            if (i == 0 || j == 0 || i == 4 || j == 4 || i == 8 || j == 8 || i == 12 || j == 12 || i == 16 || j == 16 || i == 20 || j == 20) {
+            if (i == 0 && j != 8) {
+                newDiv.classList.add("inicio");
+
+            }
+
+            if (i == 1 || j == 0 || i == 5 || j == 4 || i == 9 || j == 8 || i == 13 || j == 12 || i == 17 || j == 16 || i == 21 || j == 20) {
                 console.log("se");
                 newDiv.classList.add("camino");
 
@@ -17,17 +30,51 @@ function matrizMapa() {
 
             }
             document.getElementById("mapa").appendChild(newDiv);
-
+            mapa[i][j] = newDiv;
         }
 
     }
+
+    console.table(mapa);
 }
 
+function cargarPersonaje() {
+    //Random del enemigo
+    var XRandom = Math.floor(Math.random() * 14) + 1;
+    var YRandom = Math.floor(Math.random() * 21);
+    //Inicio del personaje
+    var horizontal = 0;
+    var vertical = 8;
+
+    mapa[horizontal][vertical].classList.add("jugador");
+    //while (mapa[XRandom][YRandom] != document.getElementByClassName("muro")) {
+    mapa[XRandom][YRandom].classList.add("chtulu");
+    //}
 
 
+}
+
+function moverPersonaje(evento) {
+    if (evento.key == "ArrowDown") {
+        MoverAbajo();
+    }
+    if (evento.key == "ArrowUp") {
+        MoverArriba();
+    }
+    if (evento.key == "ArrowRigth") {
+        MoverDerecha();
+    }
+    if (evento.key == "ArrowLefth") {
+        MoverIzquierda();
+    }
+}
+
+function MoverAbajo() {
+
+}
 window.onload = function() {
 
     matrizMapa();
-
+    cargarPersonaje();
+    moverPersonaje();
 }
-8
