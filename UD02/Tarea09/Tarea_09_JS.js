c479ab9b419bbab0c8515cb40058451ecf565fe5
KEVIN SANZ FERNANDEZ <kevindevalencia@gmail.com>
2019-10-31 20:21:36 +0100

    Terminar transicion

--- UD02/Tarea09/Tarea_09_JS.js
+++ UD02/Tarea09/Tarea_09_JS.js
@@ -1,12 +1,26 @@
+    const keys = document.querySelectorAll(".key");
 
-
+    keys.forEach( key => key.addEventListener('transitionend', removeTransition));
 
 function playSound(key){
     console.log(key);
     let audio = document.querySelector(`audio[data-key="${key.keyCode}"]`);
+    
+    if (!audio) {
+        return;
+    }
+
+    audio.currentTime = 0;
     audio.play();
+
+    let Sound = document.querySelector(`.key[data-key="${key.keyCode}"]`);
+    Sound.classList.add("Transmisionado");
 }
 
+function removeTransition(e){
+    console.log("HOLA");
+    this.classList.remove("Transmisionado");
+}
 
 
 window.onload = function () {
