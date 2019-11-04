c87eef19f3bdc442195f615021abe7742f078a97
KEVIN SANZ FERNANDEZ <kevindevalencia@gmail.com>
2019-11-04 16:12:34 +0100

    Tarea9 perfecta

--- UD02/Tarea09/Tarea_09_JS.js
+++ UD02/Tarea09/Tarea_09_JS.js
@@ -1,28 +1,30 @@
-const keys = document.querySelectorAll(".key");
-
-keys.forEach( element => element.addEventListener("transitionend", removeTransition));
 
 function playSound(key){
-const audio = document.querySelector(`audio[data-key="${key.keyCode}"]`);
+    const audio = document.querySelector(`audio[data-key="${key.keyCode}"]`);
+        
+    if (!audio) {
+        return;
+    }
 
-if (!audio) {
-    return;
-}
+    audio.currentTime = 0;
+    audio.play();
 
-audio.currentTime = 0;
-audio.play();
-
-const Sound = document.querySelector(`.key[data-key="${key.keyCode}"]`);
-Sound.classList.add("Transmisionado");
+    const Sound = document.querySelector(`.key[data-key="${key.keyCode}"]`);
+    Sound.classList.add("transmisionado");
 }
 
 function removeTransition(e){
-e.target.classList.remove("Transmisionado");
+    console.log("angel");
+    e.target.classList.remove("transmisionado");
 }
 //Por alguna razon no me quita la Transition, preguntar a angel.
 
 
 window.onload = function () {
 document.onkeypress = playSound;
+const Ourkeys = document.querySelectorAll('.key');
 
+Ourkeys.forEach(element => element.addEventListener("transitionend", removeTransition));
 }
+
+
