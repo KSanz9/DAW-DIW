99b9cf9c9ace4b6b3d0558aa72fc54df51df0ef1
Kevin Sanz Fernandez <35343881+KSanz9@users.noreply.github.com>
2019-11-24 19:59:32 +0100

    Añado funcion progressBar que es async para que el await funcione correctamente y da la sensacion que migra cosas

--- UD03/Tarea_01_Migracion/js/migracion.js
+++ UD03/Tarea_01_Migracion/js/migracion.js
@@ -4,17 +4,42 @@ Author: Angel Berlanas Vicente
 email : <berlanas_ang@gva.es>
 */
 
-function progressBar(stepEvent,stepNum) {
-    console.log("avanza la barra");
-}//No se me ocurre como hacer la barra progresiva. Preguntar a angel.
+function time(x) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(x); //funcion para hacer que se espere
+        }, 10);
+    });
+}
 
+
+async function progressBar(stepEvent,stepNum) {
+    var Newstep = document.querySelectorAll('[data-step]');
+    var cont = 0;
+    
+    for (let i = 0; i <= 100; i++) { 
+            await time(0);
+        
+        stepEvent.setAttribute('value', +i);
+    }
+
+    stepNum++;
+
+
+}
 
 function migracionFase(event){
     var stepNum = parseInt(event.target.dataset.step)+1;
     var stepEvent = document.querySelector(`[data-step="${stepNum}"]`);
     if(stepNum < 19){
         stepEvent.addEventListener("transitionend", migracionFase); 
+        if (stepEvent.localName == 'progress') {
             stepEvent.classList.add("estabaEscondido");
+            progressBar(stepEvent,stepNum);
+        }else{
+            stepEvent.classList.add("estabaEscondido");
+        }
+           
         
     }
 }
@@ -24,6 +49,7 @@ function migracionFase(event){
 function startMigration(){
     let stepdata = document.querySelector('[data-step="1"]'); 
     stepdata.addEventListener("transitionend", migracionFase);
+
     stepdata.classList.add("estabaEscondido"); 
 
     // Fragmentos perdidos
