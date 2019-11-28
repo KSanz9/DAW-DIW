ef88042f1e74a8643b39ae097d9a4774b02fd6d4
KEVIN SANZ FERNANDEZ <kevindevalencia@gmail.com>
2019-11-28 18:59:36 +0100

    Terminando botones js

--- Diego/Reproductor/JS/rep.js
+++ Diego/Reproductor/JS/rep.js
@@ -32,18 +32,42 @@ function quitarBotones(){
 
 function funcionalidadesMenu(){
     var arrayBotones = document.getElementsByClassName("botonMenu");
-
+    var boton;
     for (let i = 0; i < arrayBotones.length; i++) {
         
 
         switch (i) {
             case 0://Replay
-                
+                boton = document.getElementById("stop");
+                boton.addEventListener("click", replayVideo);
+
+                break;
+            case 1://-10 s video
+                boton = document.getElementById("prev");
+                boton.addEventListener("click", prev);
+
                 break;
             case 2://play
-                var boton = document.getElementById("play");
+                boton = document.getElementById("play");
                 boton.addEventListener("click", playVideo);
             
+                break;
+            case 3:
+                boton = document.getElementById("next");
+                boton.addEventListener("click", next);
+    
+                break;
+            case 4:
+                boton = document.getElementById("volUp");
+                boton.addEventListener("click", vol);
+                break;
+            case 5:
+                boton = document.getElementById("volDown");
+                boton.addEventListener("click", vol);
+                break;
+            case 6:
+                boton = document.getElementById("muteVol");
+                boton.addEventListener("click", vol);
                 break;
             default:
                 break;
@@ -65,4 +89,37 @@ function playVideo(){
         play = false; 
     }
     boton.classList.toggle("pause")
+}
+
+function replayVideo(){
+    var video = document.getElementById("video");
+    video.currentTime = 0;
+}
+
+function prev(){
+    var video = document.getElementById("video");
+    video.currentTime -= 10;
+}
+
+
+function next(){
+    var video = document.getElementById("video");
+    video.currentTime += 10;
+}
+
+function vol(event){
+    var video = document.getElementById("video");
+
+   //console.log(event.target);
+   //console.log(this.attributes[0]);
+    if(this.attributes[0].nodeValue == "volUp" && video.volume < 1){
+        video.volume += 0.1;
+    }
+    if(this.attributes[0].nodeValue == "volDown" && video.volume > 0){
+        video.volume -= 0.1;
+    }
+    if(this.attributes[0].nodeValue == "muteVol"){
+        video.muted = !video.muted;
+    }
+
 }
