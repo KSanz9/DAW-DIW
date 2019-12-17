d99f796c70e3b9ecf249e07236f70799d07644cd
KEVIN SANZ FERNANDEZ <kevindevalencia@gmail.com>
2019-12-17 19:27:12 +0100

    filtros mejorados

--- UD04/Tarea02_mongoFallero/public/js/falla.js
+++ UD04/Tarea02_mongoFallero/public/js/falla.js
@@ -6,34 +6,33 @@
 
 
 // Algunos valores
-
+var json;
 const fuentesUrl = "http://mapas.valencia.es/lanzadera/opendata/Monumentos_falleros/JSON ";
+let sel = document.querySelector("select");
 
 // Esta es la funcion de filtrado para
 // obtener tan solo los elementos que cumplen
 // una serie de requisitos.
-
-
-
-
 // Pasa a mayuscula el texto de propio input
 // se lanza cada vez que se realiza una insercion en
 // el texto del nombre.
-function secciones() {
-    const fetchSecciones = fetch(fuentesUrl);
-    fetchSecciones.then(sections => {
-
-        return sections.json();
-
-    }).then(secciones => {
-        const result = secciones.features;
+function secciones(tipo) {
+        sel.innerHTML="";
+        const result = json.features;
         let sec = new Set();
-        let sel = document.querySelector("select");
 
         sec.add("Todas");
-        result.forEach(section => {
-            sec.add(section.properties.seccion);
-        });
+
+        if(tipo == "Principal"){
+            result.forEach(section => {
+                sec.add(section.properties.seccion);
+            });
+        }else{
+            result.forEach(section => {
+                sec.add(section.properties.seccion_i);
+            });
+        }
+      
         sec.forEach(opciones => {
             //console.log(opciones);
             let option = document.createElement("option");
@@ -42,20 +41,12 @@ function secciones() {
             sel.appendChild(option);
         });
 
-    });
 }
 
 
-function seccionPrincipal(){
 
-    let elemento = document.querySelector("input[type=radio]");
-}
-
-
-function seccionInfantil(){
-    let elemento = document.querySelectorAll("input[type=radio]")[1];
-}
 function buscar() {
+    document.querySelector("resultados").innerHTML = "";
 
     // Obtenemos el JSON que esta definido
     const fetchPromesa = fetch(fuentesUrl);
@@ -67,23 +58,33 @@ function buscar() {
         // Y entonces
     }).then(respuesta => {
         // Filtramos los resultados con el filtro definido anteriormente
-
-        console.log(respuesta.features[1].properties);
-
+        json = respuesta;
         let filtroSecciones = document.querySelector("select").value;
         let resultado;
         //console.log(filtroSecciones);
-
+        let radio = document.querySelector("input[name=tipoFalla]:checked");
+       
         //filtro para el select
-        if (filtroSecciones == "Todas") {
-            resultado = respuesta.features;
-        } else {
-            resultado = respuesta.features.filter(filtroSection);
-        }
+        if(radio.value == "Principal"){
+            if (filtroSecciones == "Todas") {
+                resultado = respuesta.features;
+            } else {
+                resultado = respuesta.features.filter(filtroSection);
+            }
+            secciones(radio.value);
+        }else{
+            if (filtroSecciones == "Todas") {
+                resultado = respuesta.features;
+            } else {
+                resultado = respuesta.features.filter(filtroSectionI);
+            }
+            secciones(radio.value);
 
+        }
+        
         // Una vez tenemos el listado filtrado pasamos a crear
         // cada uno de los  que representan
-
+        var img;
         
         // Por cada uno de ellos 
         resultado.forEach(falla => {
@@ -94,8 +95,8 @@ function buscar() {
 
 
             // Creamos un img y el texto de la falla
-
-            imgFalla.src = falla.properties.boceto;
+            if(radio.value == "Principal"){img =falla.properties.boceto;}else{img =falla.properties.boceto_i;}
+            imgFalla.src = img;
             textoFalla.innerText = falla.properties.nombre;
 
 
@@ -110,7 +111,6 @@ function buscar() {
             fallaValencia.appendChild(imgFalla);
             fallaValencia.appendChild(textoFalla);
             fallaValencia.appendChild(botonUbi);
-            console.log(fallaValencia);
 
             document.querySelector("resultados").appendChild(fallaValencia);
         });
@@ -123,10 +123,18 @@ function mostrarFallas() {
 
 
 }
+
+
+function filtroSectionI(elemento) {
+    //primero
+    //selecionamos la opcion del select 
+    let seccioncita = document.querySelector("select").value;
+    //devuelve el elemento de la seccion seleccionada
+    return elemento.properties.seccion_i.startsWith(seccioncita);
+}
+
 function filtroSection(elemento) {
     //primero
-    document.querySelector("resultados").innerHTML = "";
-
     //selecionamos la opcion del select 
     let seccioncita = document.querySelector("select").value;
     //devuelve el elemento de la seccion seleccionada
@@ -134,8 +142,9 @@ function filtroSection(elemento) {
 }
 
 function init() {
-    secciones();
- 
+    document.querySelectorAll("input[type=radio]").forEach(radio => {
+        radio.addEventListener("click", buscar); 
+    });
     // Binding de los eventos correspondientes.
     document.querySelector("select").addEventListener("change", buscar);
     // Click en el boton de buscar
