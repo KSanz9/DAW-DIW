f569106973630390615e4b205d8532b408c75f44
KEVIN SANZ FERNANDEZ <kevindevalencia@gmail.com>
2020-02-13 19:54:50 +0100

    añado sprites con eventos de over y out

--- Pokedex-Proyecto/public/js/poke.js
+++ Pokedex-Proyecto/public/js/poke.js
@@ -17,6 +17,7 @@ function filtroPokemon(poke){
 }
 
 function escribeFiltra(){
+    //console.log(jsonPokemon.results.filter(filtroPokemon));
     cargarPokemons(jsonPokemon.results.filter(filtroPokemon));
 }
 
@@ -33,6 +34,9 @@ function conseguirPokedex(){
     })  
   }
 
+
+
+
   function cargarPokemons(jsonPokes){
     console.log(jsonPokes);
             
@@ -41,19 +45,70 @@ function conseguirPokedex(){
 
     jsonPokes.forEach(element => {
          var poke = document.createElement("p");
-         poke.textContent = element.name;
+         poke.textContent = element.name.toUpperCase();
          pokemons.appendChild(poke);
+         
+
+         poke.id = poke.innerText.toLowerCase();
          poke.addEventListener("click", mostrarPokemon);
+         poke.addEventListener("mouseover", mostrarSprites);
+         poke.addEventListener("mouseout", quitarSprites);
      });
 
   }
 
+  function cargarSprites(urlSprite){
+
+      return fetch(urlSprite)
+      .then(respuesta => {
+          return respuesta.json();
+      }).then(newRespuesta =>{
+          //console.log(newRespuesta.sprites.front_default);
+          return newRespuesta.sprites.front_default;
+      })
+
+  }
+
+
   function mostrarPokemon(e){
       let mostrar = document.getElementById("PokemonMuestra");
         console.log(e.target.innerHTML);
-      mostrar.style.backgroundImage = "../img/pantallaPokedex.jpg";
   }
 
+  async function mostrarSprites(e){
+    
+    console.log(e.target.id);
+    var img;
+   /*  jsonPokemon.results.forEach(element =>{
+      if(element.name == e.target.id ){
+        const newUrl = element.url;
+        img = cargarSprites(newUrl);
+      }
+    }); */
+
+    for (let index = 0; index <  jsonPokemon.results.length; index++) {
+      if(jsonPokemon.results[index].name == e.target.id ){
+        const newUrl = jsonPokemon.results[index].url;
+        img = await cargarSprites(newUrl);
+      }
+    }
+      let divSprite = document.createElement("div");
+      divSprite.id = "imgDivhover";
+      let imgSprite = document.createElement("img");
+      imgSprite.src = img;
+
+      divSprite.appendChild(imgSprite);
+
+      e.target.appendChild(divSprite);
+
+
+  }
+
+
+  function quitarSprites(e){
+    console.log(e.target.children[0])
+    e.target.children[0].remove();
+  }
 function darEventos(){
     document.getElementById("cargando").style.display = "none";
     document.getElementById("contenedorPrincipal").style.display= "inherit";
